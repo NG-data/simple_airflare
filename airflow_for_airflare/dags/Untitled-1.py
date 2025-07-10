811a09355270fb2c8ed0feaf7282c7483b07e349
Nikita <nikgavrilov2013@gmail.com>
2025-07-10 19:16:24 +0800

    Нормализована ы данные в БД

--- airflow_for_airflare/dags/Untitled-1.py
+++ airflow_for_airflare/dags/Untitled-1.py
@@ -6,54 +6,36 @@ from airflow.providers.postgres.hooks.postgres import PostgresHook
 # Устанавливает соединение с S3
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow import DAG, models
-from airflow.decorators import dag, task
-import datetime as dt
 from airflow.operators.python import PythonOperator
+from airflow.decorators import dag, task
+from datetime import datetime, timedelta
 import logging  # Логирование
 import json  # Сериализация и десириализация json в строку для загрузки в S3
-# Говорит «Это не путь, а строка — обращайся с ней как с файлоподобным объектом».
-from io import StringIO
 
 # %%
-IATA_translate = {
-    'IKT': 'Иркутск',
-    'HKT': 'Пхукет',
-    'MOW': 'Москва',
-    'AER': 'Сочи',
-    'LED': 'Санкт_Петербург',
-    'VVO': 'Владивосток',
-    'MLE': 'Мале',
-    'DXB': 'Дубай',
-    'IST': 'Стамбул',
-    'AYT': 'Анталья',
-    'NHA': 'Нячанг',
-    'SGN': 'Хошимин',
-    'HAN': 'Ханой',
-    'TYO': 'Токио',
-    'KUL': 'Куала_Лумпур',
-    'PKX': 'Пекин_Дасин',
-    'PEK': 'Пекин_Шоуду'
-}
-
+hook = PostgresHook(postgres_conn_id='postgres_airfare')
+engine = hook.get_sqlalchemy_engine()
+iata_df = pd.read_sql('SELECT id, iata FROM iata_codes', con=engine)
+iata_dict = dict(zip(iata_df['iata'], iata_df['id']))
 
 # %%
-city_list = ['HKT', 'MOW', 'AER', 'LED', 'VVO', 'MLE', 'DXB', 'IST', 'AYT',
-             'NHA', 'SGN', 'HAN', 'TYO', 'KUL', 'PKX', 'PEK']  # Города назначения
-origin_city = 'IKT'  # Город вылета
+city_list = ['IKT', 'HKT', 'MOW', 'AER', 'LED', 'VVO', 'MLE', 'DXB', 'IST', 'AYT',
+             'NHA', 'SGN', 'HAN', 'TYO', 'KUL', 'PKX', 'PEK', 'RGK']  # Города назначения
+iata = {code: i for i, code in enumerate(city_list)}
 routs = []
 
-for i in city_list:  # Генерирует комбинацию билетов туда-обратно
-    city_dickt = {}
-    city_dickt['origin'] = origin_city
-    city_dickt['destination'] = i
-    city_dickt['bd'] = f'{IATA_translate[origin_city]}_{IATA_translate[i]}_outbound'
-    routs.append(city_dickt)
+for i in range(1, len(city_list)):  # Генерирует комбинацию билетов туда-обратно
+    city_dict = {}
+    city_dict['origin'] = city_list[0]
+    city_dict['destination'] = city_list[i]
+    city_dict['bd'] = 'ikt_airflare_outbound'
+    routs.append(city_dict)
 
-    city_dickt = {}
-    city_dickt['origin'] = i
-    city_dickt['destination'] = origin_city
-    city_dickt['bd'] = f'{IATA_translate[i]}_{IATA_translate[origin_city]}_return'
-    routs.append(city_dickt)
+    city_dict = {}
+    city_dict['origin'] = city_list[i]
+    city_dict['destination'] = city_list[0]
+    city_dict['bd'] = 'ikt_airflare_return'
+    routs.append(city_dict)
 
 
 # %%
@@ -62,15 +44,15 @@ def upload_to_s3(data, key, bucket='airflare'):
     logging.info('Создание подключения к S3')
     hook = S3Hook(aws_conn_id='minio')  # Установка подключения к S3
 
-    logging.info('Подключение установлено')
-
-    # Проверяет есть ли бакет с таким именем
-    if not hook.check_for_bucket(bucket):
-        hook.create_bucket(bucket_name=bucket)
+    try:
+        # Проверяет есть ли бакет с таким именем
+        if not hook.check_for_bucket(bucket):
+            hook.create_bucket(bucket_name=bucket)
+        logging.info('Подключение установлено')
+    except Exception as e:  # Логирование ошибки
+        logging.error(f'Ошибка подключения к minio, {e}')
 
     logging.info('Загрузка данных в S3')
-
-    key = f"temp/{key}"
 
     hook.load_string(  # Загружает строку в S3
         string_data=data,  # Сериализованные в строку данные
@@ -91,12 +73,19 @@ def upload_to_s3(data, key, bucket='airflare'):
 def load_from_s3(bucket, key, aws_conn_id='minio'):
     logging.info('Создание подключения к S3')
     # Задает переменную подключения где aws_conn_id это id подключения к S3 в UI airflow
-    conn = S3Hook(aws_conn_id=aws_conn_id)
+    hook = S3Hook(aws_conn_id=aws_conn_id)
+    try:
+        # Проверяет есть ли бакет с таким именем
+        if not hook.check_for_bucket(bucket):
+            hook.create_bucket(bucket_name=bucket)
+        logging.info('Подключение установлено')
+    except Exception as e:
+        logging.error(f'Ошибка подключения к minio, {e}')
 
     logging.info('Подключение установлено, загрузка данных из S3')
 
     # Читает сериализованный файл в заданном бакете по заданному пути
-    content = conn.read_key(bucket_name=bucket, key=key)
+    content = hook.read_key(bucket_name=bucket, key=key)
 
     data = json.loads(content)  # Десериализует файл
     logging.info('Данные выгружены из S3')
@@ -106,9 +95,9 @@ def load_from_s3(bucket, key, aws_conn_id='minio'):
 # %%
 default_args = {
     'owner': 'airflow',
-    'start_date': dt.datetime(2024, 1, 1),
+    'start_date': datetime(2024, 1, 1),
     'retries': 2,
-    'retry_delay': dt.timedelta(minutes=1)
+    'retry_delay': timedelta(minutes=1)
 }
 
 with DAG(dag_id='ETL_airflare_data', default_args=default_args, schedule_interval='0 12 * * *', catchup=False, tags=['cost']) as dag:
@@ -144,11 +133,10 @@ with DAG(dag_id='ETL_airflare_data', default_args=default_args, schedule_interva
             raise ValueError('Ошибка')
 
         # Полученный ответ оборачивает в JSON затем сериализуется в строку и загружается по заданному пути в S3
-        path = upload_to_s3(data=json.dumps(
-            response.json()), key=f"response_{rout['origin']}_{rout['destination']}.json")
+        path = upload_to_s3(data=json.dumps(response.json(
+        )), key=f"row_airflare_data_by_day/response_{rout['origin']}_{rout['destination']}_{str(datetime.today().date())}.json")
 
-        logging.info('Данные успешно сохранены во временное хранилище')
-        logging.info('Данные успешно извлечены и переданы в Xcom')
+        logging.info('Данные успешно сохранены в S3')
 
         return {'path': path, 'rout': rout}
 
@@ -157,55 +145,81 @@ with DAG(dag_id='ETL_airflare_data', default_args=default_args, schedule_interva
     def transform(info):  # Функция оставляет только нужные нам данные
         path = info['path']
         rout = info['rout']
-        logging.info('Данные приняты из Xcom, начат процесс очистки')
+        logging.info('Извлечение данных из S3, подготовка к обработке')
 
         # Выгружает файл из S3 и десериализует его
         file = load_from_s3(bucket=path['bucket'], key=path['key'])
+
+        logging.info('Начата обработка данных')
 
         df = pd.DataFrame(file['data'])
         df = df[['depart_date', 'origin', 'destination', 'trip_class',
                 'value', 'gate', 'duration', 'distance', 'number_of_changes']]
 
-        logging.info('Преобразование завершено, начинается передача DF в S3')
+        cur_date = pd.Series([datetime.date(datetime.today())] * len(
+            df), name='date_of_extraction').astype('str')  # Создает серию хранящую текущую дату
+
+        # Создает серию хранящую время до вылета
+        day_before_departure = pd.to_datetime(
+            df['depart_date']) - pd.to_datetime(cur_date)
+        day_before_departure.name = 'day_before_departure'
+        day_before_departure = day_before_departure.map(lambda x: x.days)
+
+        df = pd.concat([df, cur_date, day_before_departure], axis=1)
+
+        # замена IATA кодов на индексы для нормализации данных
+        df['origin'] = df['origin'].map(iata_dict)
+        df['destination'] = df['destination'].map(iata_dict)
+        upload_to_s3(df.to_json(index=False, orient='records'), 'df_now.json')
+        logging.info(df['date_of_extraction'].dtype)
+        logging.info(df['depart_date'].dtype)
+
+        logging.info('Обработка завершена, начинается передача DF в S3')
 
         # Преобразует DF в JSON
         content = df.to_json(index=False, orient='records')
 
         path = upload_to_s3(
-            content, bucket=path['bucket'], key=f"df_{rout['origin']}_{rout['destination']}.json")  # Загружает в S3
+            content, bucket=path['bucket'], key=f"DataFrames/df_{rout['origin']}_{rout['destination']}.json")  # Загружает в S3
 
-        logging.info('Данные успешно сохранены в S3, передаю их в Xcom')
+        logging.info('Данные успешно сохранены в S3')
 
-        logging.info('данные успешно очищены и записаны в хранилище')
         return {'path': path, 'rout': rout}
 
 # %%
     @task
-    def load(info):  # Функция загружает данные в БД
-
-        logging.info(
-            'Данные из Xcom успешно выгружены, начинаю выгружать данные из S3')
-
+    def load_to_postgres(info):  # Функция загружает данные в БД
         path = info['path']  # Путь до файла в S3
         rout = info['rout']  # Текущее направление
-
         # Выгружает из S3 файл который внутри содержит список словарей
         content = load_from_s3(bucket=path['bucket'], key=path['key'])
 
-        df = pd.DataFrame(list(content))
         logging.info(
             'Данные получены из хранилища, начат процесс отправки в БД')
+
+        df = pd.DataFrame(list(content))
 
         # Указывать имя в airflow UI при соединении с БД которое указано в yaml(postgres_conn_id= это id в aiflow UI)
         hook = PostgresHook(postgres_conn_id='postgres_airfare')
         engine = hook.get_sqlalchemy_engine()
+        logging.info('Проверяю подключение к postgres')
+        try:  # Проверяем подключение к postgres
+            # Получаем подключение
+            conn = hook.get_conn()
+
+            # Создаем курсор для выполнения простого sql запроса для проверки соединения
+            with conn.cursor() as cursor:
+                cursor.execute("SELECT 1;")  # Выполняет запрос к БД
+                result = cursor.fetchone()  # Возвращает результат запроса
+                print("Соединение с PostgreSQL успешно. Результат запроса:", result)
+        except Exception as e:
+            print("Ошибка соединения с PostgreSQL:", e)
 
         # Выводит в логи размерность DF
         logging.info(f'Размер DataFrame: {df.shape}')
         # Выводит в логи типы данных каждого столбца DF
         logging.info(f'Типы данных:\n{df.dtypes}')
 
-        logging.info('Связь с БД установлена')
         try:
             # Отправляет данные в заданную таблицу
             df.to_sql(f'{rout['bd']}', engine, if_exists='append', index=False)
@@ -217,4 +231,4 @@ with DAG(dag_id='ETL_airflare_data', default_args=default_args, schedule_interva
 # %%
     extract_args = extract.expand(rout=routs)
     transform_args = transform.expand(info=extract_args)
-    load.expand(info=transform_args)
+    load_to_postgres.expand(info=transform_args)
